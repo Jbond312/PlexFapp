1b8858a72dac91cc7aa34805ef8f2019e1ed4e0b
James Bond <james@bonton.io>
2020-02-29 15:14:19 +0000

    Added anonymous function

--- PlexFapp/Function1.cs
+++ PlexFapp/Function1.cs
@@ -28,5 +28,23 @@ namespace PlexFapp
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
+
+        [FunctionName("Function2")]
+        public static async Task<IActionResult> RunAnonymous(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello anonymous user, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
     }
 }
