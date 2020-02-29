64809c92c3dfb06d7b975211124bdf724f307d25
James Bond <james@bonton.io>
2020-02-29 15:16:56 +0000

    Added an admin level function too

--- PlexFapp/Function1.cs
+++ PlexFapp/Function1.cs
@@ -25,13 +25,31 @@ namespace PlexFapp
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello function user, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
 
         [FunctionName("Function2")]
         public static async Task<IActionResult> RunAnonymous(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello anonymous user, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+        [FunctionName("Function3")]
+        public static async Task<IActionResult> RunAdmin(
+            [HttpTrigger(AuthorizationLevel.Admin, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
